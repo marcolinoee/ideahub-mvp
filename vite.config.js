b3676bf173c7a288133887b3bd81d8a8345709f6
Anderson Marcolino <marcolinoee@google.com>
2025-11-23 00:48:09 -0300

    Fix: Caminhos absolutos para icones PWA

--- vite.config.js
+++ vite.config.js
@@ -9,40 +9,36 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'IdeaHub - Soluções Comunitárias',
+        name: 'IdeaHub',
         short_name: 'IdeaHub',
-        description: 'Conectando comunidade e universidade para resolver problemas reais.',
+        description: 'Conectando comunidade e universidade.',
         theme_color: '#0B1120',
         background_color: '#ffffff',
         display: 'standalone',
+        scope: '/',
+        start_url: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png', // Adicionamos a barra / no inicio
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png', // Adicionamos a barra / no inicio
             sizes: '512x512',
             type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
           }
         ],
         screenshots: [
           {
-            src: "pwa-512x512.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             form_factor: "wide",
             label: "Desktop"
           },
           {
-            src: "pwa-512x512.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             form_factor: "narrow",
